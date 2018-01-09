f28e7a273828289bfbe7f507920fabeb1978b68b
Joshua <joshstockin@gmail.com>
2018-01-08 18:21:55 -0600

    Getting started + OpenTK reference
    Basic references, console controls, OpenTK import

---
+++ InstantGameworks/Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using Global.Services;
+using System.Windows.Forms;
+
+namespace Global
+{
+
+    class Init
+    {
+
+        private static void DebugWriteLine(string output) => Console.WriteLine(SysTime.GetTime() + " " + output);
+
+        [STAThread]
+        public static void Main()
+        {
+
+            Console.WriteLine("Instant Gameworks (c)2018");
+            DebugWriteLine("Init");
+
+
+            //Main
+
+
+
+            DebugWriteLine("Running");
+            DebugWriteLine("Shutting down");
+
+
+        }
+
+    }
+
+}
---
+++ InstantGameworks/Services/SysTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global.Services
+{
+    class SysTime
+    {
+
+        public static string GetTime()
+        {
+            DateTime datetime = DateTime.Now;
+            string CurrentTime = "";
+            CurrentTime += string.Format("{0:0}:{1:00}:{2:00}.{3:000}", datetime.Hour, datetime.Minute, datetime.Second, datetime.Millisecond);
+            return CurrentTime;
+        }
+
+    }
+}
